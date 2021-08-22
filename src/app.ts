14c6471ee4ec07e155bb8330fe2ea3d4fd855af9
Ananta Pratama <deras.hujan15@gmail.com>
2021-08-22 14:22:32 +0700

    Add https config

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,27 @@
 import express from "express";
+import fs from "fs";
+import https from "https";
 
 import { loadApp } from "@/loaders";
 import config from "@/config";
 
 async function startServer() {
   const app = express();
+
+  if (config.nodeEnv === "production") {
+    const key = fs.readFileSync("selfsigned.key");
+    const cert = fs.readFileSync("selfsigned.crt");
+    https
+      .createServer(
+        {
+          key: key,
+          cert: cert,
+        },
+        app
+      )
+      .listen(config.httpsPort);
+    console.log("HTTPS server started at: ", config.httpsPort);
+  }
 
   await loadApp(app);
   app.listen(config.port);
